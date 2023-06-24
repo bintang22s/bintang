c4c5fc2c63ddb4ca76621cac6e73e9ade582ed0e
bintang22s <bintangmf00@gmail.com>
2023-06-24 17:06:50 +0700

    fresh

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+import 'bulma/css/bulma.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from  './pages/Home';
+import About from './pages/About';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+       
+          
+          <Route path="home" element={<Home />} />
+          <Route path="About" element={<About />} />
+          
+       
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
